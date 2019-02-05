aa970d1af467bd431b3138887acddfa7834c0225
no <no@42.fr>
2019-02-05 11:19:51 +0100

    test with good variables name

--- test.sh
+++ test.sh
@@ -17,26 +17,26 @@ if [ $? -ne 0 ] ; then
 fi
 
 cargo build --release
+LOG_WORDS="len\|cpu\|number\|state"
+COMMIT_ID="`git rev-parse master | head -c 8`"
+TIME="`date +"%s"`"
+FILE_NAME="time_test_$1_${TIME}:${COMMIT_ID}.log"
 
-commit_id="`git rev-parse master | head -c 8`"
-time="`date +"%s"`"
-file_name="time_test_$1_${time}:${commit_id}.log"
-
-echo "#########################################################################" > ${file_name}
-echo "# commit: ${commit_id}" >> ${file_name}
-echo "#" >> ${file_name}
-echo "# for e in \`ls test/solvable$1\*\` ; do" >> ${file_name}
-echo "#	echo \"\\\n\$e:\" ;" >> ${file_name}
-echo '#	{ time ./target/release/npuzzle $e } 2>&1 | grep -i "len\|cpu\|number\|state";' >> ${file_name}
-echo '# done  > ${file_name}' >> ${file_name}
-echo "#" >> ${file_name}
-echo "#" >> ${file_name}
-echo "# # cat $file_name | grep target | awk -F ' ' '{print $3}'" >> ${file_name}
-echo "#########################################################################\n\n" >> ${file_name}
+echo "#########################################################################" > ${FILE_NAME}
+echo "# commit: ${COMMIT_ID}" >> ${FILE_NAME}
+echo "#" >> ${FILE_NAME}
+echo "# for e in \`ls test/solvable$1\*\` ; do" >> ${FILE_NAME}
+echo "#	echo \"\\\n\$e:\" ;" >> ${FILE_NAME}
+echo "#	{ TIME ./target/release/npuzzle $e } 2>&1 | grep -i \"${LOG_WORDS}\";" >> ${FILE_NAME}
+echo '# done  > ${FILE_NAME}' >> ${FILE_NAME}
+echo "#" >> ${FILE_NAME}
+echo "#" >> ${FILE_NAME}
+echo "# # cat $FILE_NAME | grep target | awk -F ' ' '{print \$3}'" >> ${FILE_NAME}
+echo "#########################################################################\n\n" >> ${FILE_NAME}
 
 for e in `ls test/solvable$1*` ; do
 	echo "\n$e:" ;
-	{ time ./target/release/npuzzle $e } 2>&1 | grep -i 'len\|cpu\|number\|state';
- done >> ${file_name}
+	{ time ./target/release/npuzzle $e } 2>&1 | grep -i "${LOG_WORDS}";
+ done >> ${FILE_NAME}
 
-echo "${file_name} generate"
+echo "${FILE_NAME} generate"
