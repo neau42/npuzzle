52dc8029fd193faf197cfad8421da49fbd589144
no <no@42.fr>
2019-02-12 21:12:39 +0100

    print ok

--- src/print.rs
+++ src/print.rs
@@ -10,6 +10,8 @@ use crate::options::Options;
 
 pub fn	print(close_list: &HashSet<RefPuzzle>, mut puzzle: &RefPuzzle, final_state: &puzzle::FinalPuzzle, opts: &Options) {
 
+	let ref mut end: Vec<Vec<u16>> = Vec::new();
+
 	// let mut next = &close_list.get(puzzle).unwrap().ref_puzzle.borrow().predecessor;
 
 	// let mut nx = match next {
@@ -24,21 +26,35 @@ pub fn	print(close_list: &HashSet<RefPuzzle>, mut puzzle: &RefPuzzle, final_stat
 	loop {
 		let mut next = match &test1.predecessor {
 			Some(ref_next) => ref_next,
-			_ 			=> return,
+			_ 			=> break,
 		};
 		let new1 = close_list.get(&next).unwrap();
 		test1 = new1.ref_puzzle.borrow();
 		let a = &test1.taq;
-		puzzle::print_puzzle(&a , final_state, opts);
-		println!("~~");
+		// puzzle::print_puzzle(&a , final_state, opts);
+		end.push(a.clone());
+		// println!("~~");
 	}
-	// dbg!(a);
-
-	// println!("{}");
-	
-
-	// puzzle::print_puzzle(&next , final_state, opts);
-	// puzzle = close_list.get(&nx).unwrap();
+	let mut len: u32 = 0;
+	let mut elem: Vec<u16>;
+	loop {
+		elem = match end.pop() {
+			Some(x) => x,
+			_       => break, 
+		};
+		if opts.sleep {
+			println!("{}[2J{}",27 as char , termion::cursor::Goto(1, 1));
+		}
+		println!("N-puzzle: ");
+		puzzle::print_puzzle(&elem, final_state, opts);
+		if opts.sleep {
+			thread::sleep(time::Duration::from_millis(200));
+		}
+		len += 1;
+	}
+	let c_list_len = close_list.len();
+	println! ("nb movements: {} ", len);
+	println! ("Close list length : {} ", c_list_len);
 }
 
 pub fn	print_all(p: & Vec<u16>, close_list: &HashMap<Vec<u16>, (i32, i32, Vec<u16>)>,
@@ -58,6 +74,8 @@ prev: &Vec<u16>, final_state: &puzzle::FinalPuzzle, opts: &Options) -> u32 {
 		};
 		len += 1;
 	}
+
+	let mut len: u32 = 0;
 	let mut elem: Vec<u16>;
 	loop {
 		elem = match end.pop() {
