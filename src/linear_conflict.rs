dcea1ee23f1fa7dae6e9f0df6c112ddf57bbb9eb
no <no@42.fr>
2019-02-15 20:16:51 +0100

    clippy

--- src/linear_conflict.rs
+++ src/linear_conflict.rs
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   linear_conflict.rs                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: no <no@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: nboulaye <nboulaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 10:34:16 by no                #+#    #+#             */
-/*   Updated: 2019/02/15 19:15:53 by no               ###   ########.fr       */
+/*   Updated: 2019/02/15 20:13:21 by nboulaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,14 @@ pub fn distance_estimator(taquin: &[u16], final_state: &puzzle::FinalPuzzle) -> 
         final_pos_y = final_state.position[current_value] as usize / size;
         if current_pos_y == final_pos_y {
             //same line
-			// for i2 in taquin.iter().take(final_pos_y * size + size).skip(i + 1) {
-            for i2 in (i + 1)..(final_pos_y * size + size) {
-                if taquin[i2] != 0
-                    && final_state.position[taquin[i2] as usize] as usize / size == current_pos_y
-                    && final_state.position[taquin[i2] as usize] as usize % size < final_pos_x
+			for elem in taquin.iter().take(final_pos_y * size + size).skip(i + 1) {
+				 if *elem != 0
+                    && final_state.position[*elem as usize] as usize / size == current_pos_y
+                    && final_state.position[*elem as usize] as usize % size < final_pos_x
                 {
                     cmpt += 2;
                 }
-            }
+			}
         }
         if current_pos_x == final_pos_x {
             //same raw
