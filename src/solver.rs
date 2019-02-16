79314de4bbf641c6040246dd83d1c0ece3e7a4f5
no <no@42.fr>
2019-02-16 22:01:33 +0100

    makefile

--- src/solver.rs
+++ src/solver.rs
@@ -6,7 +6,7 @@
 /*   By: nboulaye <nboulaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 10:34:10 by no                #+#    #+#             */
-/*   Updated: 2019/02/16 18:04:19 by nboulaye         ###   ########.fr       */
+/*   Updated: 2019/02/16 21:56:03 by nboulaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,6 @@ fn update_open_list(
 }
 
 pub fn solve(first: Vec<u16>, final_state: &puzzle::FinalPuzzle, opts: &Options) {
-    // let mut close_list: HashSet<RefPuzzle> = HashSet::new();
     let mut open_list: BinaryHeap<RefPuzzle> = BinaryHeap::new();
     let mut all_list: HashSet<RefPuzzle> = HashSet::new();
     let mut puzzle = RefPuzzle::first(first, final_state, opts);
@@ -65,7 +64,6 @@ pub fn solve(first: Vec<u16>, final_state: &puzzle::FinalPuzzle, opts: &Options)
 
     let start = Instant::now();
     all_list.insert(puzzle.clone());
-    // close_list.insert(puzzle.clone());
 
     while puzzle.ref_puzzle.borrow().taq != final_state.puzzle {
         update_open_list(
@@ -78,9 +76,7 @@ pub fn solve(first: Vec<u16>, final_state: &puzzle::FinalPuzzle, opts: &Options)
         );
         puzzle = open_list.pop().unwrap();
         actual_dst = puzzle.ref_puzzle.borrow().actual_dst as usize;
-
-	puzzle.ref_puzzle.borrow_mut().in_close = true;
-        // close_list.insert(puzzle.clone());
+		puzzle.ref_puzzle.borrow_mut().in_close = true;
     }
     let time = start.elapsed();
     println!("Success :)");
